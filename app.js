75bf206e14314bc856afba6a21be021d7bb404f7
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2024-05-18 05:03:59 +0400

    finished

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const Secretnumber = Math.trunc(Math.random() * 20) + 1;
+let Secretnumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-
+let highscore = 0;
 // document.querySelector('.number').textContent = Secretnumber
 
 document.querySelector(".check").addEventListener("click", function(){
@@ -18,6 +18,13 @@ document.querySelector(".check").addEventListener("click", function(){
         document.querySelector(".message").textContent = "🎉 Correct Number!"
         document.querySelector("body").style.backgroundColor = '#60b347'
         document.querySelector(".number").style.width = '30rem'
+
+        if(score > highscore){
+            highscore = score;
+            document.querySelector(".highscore").textContent = highscore
+
+        }
+
     }else if(guess > Secretnumber){
         if(score > 1){
             document.querySelector('.message').textContent = '📈 Too high'
@@ -36,4 +43,16 @@ document.querySelector(".check").addEventListener("click", function(){
             document.querySelector('.message').textContent = '💥 You lost the game'
         }
     }
+})
+
+document.querySelector(".again").addEventListener("click", function(){
+  score = 20;
+   Secretnumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".guess").value = ''
+  document.querySelector('.score').textContent = score;
+  document.querySelector("body").style.backgroundColor = '#222'
+  document.querySelector('.message').textContent = 'Start guessing...'
+  document.querySelector(".number").style.width = '15rem'
+  document.querySelector('.number').textContent = '?'
+
 })
