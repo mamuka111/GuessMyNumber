802324cc94bde56676a3ad18616e971947e2c1ce
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2024-05-18 04:38:21 +0400

    js

---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const Secretnumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+// document.querySelector('.number').textContent = Secretnumber
+
+document.querySelector(".check").addEventListener("click", function(){
+    const guess = Number(document.querySelector(".guess").value)
+    console.log(guess);
+
+
+    if(!guess){
+        document.querySelector(".message").textContent = "⛔ No Number!"
+    }else if (guess === Secretnumber){
+        document.querySelector('.number').textContent = Secretnumber
+        document.querySelector(".message").textContent = "🎉 Correct Number!"
+        document.querySelector("body").style.backgroundColor = '#60b347'
+        document.querySelector(".number").style.width = '30rem'
+    }else if(guess > Secretnumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = '📈 Too high'
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = '💥 You lost the game'
+        }
+       
+    }else if(guess < Secretnumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = '📉 Too low'
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = '💥 You lost the game'
+        }
+    }
+})
